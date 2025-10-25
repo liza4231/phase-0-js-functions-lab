6dd14a3a7083e66e61232649875dc64692e5fee3
Elizabeth Salazar <liza4231@gmail.com>
2025-10-24 19:49:35 -0500

    1st attempt

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+function calculateTax(amount) {
+    return amount * .1
+}
 
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+
+function isPalindrome(word) {
+    let backword = '';
+    for (let i = word.length -1; i>=0; i--) {
+        backword += word[i];
+    }
+    if (word === backword) {
+        return true;
+    } else {
+        return false;
+    }
+}
+  
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice * (1 - (discountPercentage / 100))
+}
 
 
 // This is required for the test to function properly  
